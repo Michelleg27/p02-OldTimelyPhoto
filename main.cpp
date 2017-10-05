7f68d85e22593ca9097597d8763939b10c724ab3
Michelleg27 <mgomez48@mail.csuchico.edu>
2017-10-04 18:07:46 -0700

    Commented what needs to be done

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include "bitmap.h"
+
+
+int main()
+{
+//ask user for the name of the an image
+    //cout question and cin the users input
+    //storage users input
+//covert image 
+
+//save file - oldtimey.bmp
+    
+
+
+
+
+
+
+
+return 0;
+}
