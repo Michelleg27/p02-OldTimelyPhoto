9480a9c96f90660329c19cde3f28b38a89af6b81
Michelleg27 <mgomez48@mail.csuchico.edu>
2017-10-16 23:05:28 -0700

    P02

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string>
 #include <vector>
 #include "bitmap.h"
@@ -10,25 +11,50 @@ int main()
     //cout question and cin the users input
     //storage users input
 //covert image 
-
 //save file - oldtimey.bmp
 
 
+string input;
+vector<vector<int> >matrix;
+void open (std::string);
+void save(std::string);
 
- 
-Bitmap image;
+cout<<"What is the name of the image you want to see"<<endl; 
+cin>>input;
+
+for(int row=0; row<matrix.size(); row++)
+    {
+        for(int col=0; col<matrix[row].size(); col++)
+            {
+            cout<<"It is 2486 pixels wide and 532 pixels high"<<endl; 
+           
+             }
+     }         
 
   Pixel purpleDot;
   purpleDot.red = 255;
   purpleDot.green = 0;
   purpleDot.blue = 255;
 
+//rgb =matrix[row][col]; 
+//rgb.red = 30;
+//rgb.green = 10;
+// rgb.blue = 50;
+
+
+
+
+Bitmap image;
 
   vector <vector <Pixel> > bmp;
   Pixel rgb;
 
   image.open("machupicchu.dib.bmp");
-  bmp = image.toPixelMatrix();
+  bool validBmp = image.isImage();
+  if (validBmp == true)
+    {
+        bmp = image.toPixelMatrix();
+
 
   rgb = bmp [0] [0];
   rgb.red = 0;
@@ -38,7 +64,8 @@ Bitmap image;
   image.save("machupicchu.dib.bmp");
 
   std::vector <std::vector <Pixel> > toPixelMatrix();
+void fromPixelMatrix(const std::vector <std::vector <Pixel> > &);
 
-
+    }
    return 0;
 }
